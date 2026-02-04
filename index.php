f87e64e7454cebd3608dcb972112566fff104518
johneeeeeeeel <malbino.febyjohnrel.onms@gmail.com>
2026-02-04 22:05:39 +0800

    Test 1

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-    echo "Hello, World!";
-    echo "This is a sample PHP file.";
-?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F.J Portfolio</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
