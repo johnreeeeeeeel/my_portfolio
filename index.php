0f8c2b375807dfabc936aa4094b1d364164d5c6f
johneeeeeeeel <malbino.febyjohnrel.onms@gmail.com>
2026-04-22 10:20:55 -0400

    Fixed Infinityfree Hosting & Added Versions Folder

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>F. Johnrel - Portfolio</title>
+    <title>F. Johnrel - Portfolio</title> 
+    
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
